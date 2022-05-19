76cb5bae32ab8c23a412f5446a2b7830f748cb43
Desmond Kyeremeh <elDekyfin@gmail.com>
2022-05-19 23:47:43 +0000

    Disable firefox PPA

--- setup-firefox.sh
+++ setup-firefox.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apt update
-apt install software-properties-common -y
-add-apt-repository ppa:mozillateam/ppa
-apt update
+# apt install software-properties-common -y
+# add-apt-repository ppa:mozillateam/ppa
+# apt update
 apt install firefox-esr -y
